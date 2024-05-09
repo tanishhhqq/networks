419a3b000799f12ac5a8340b48b9bc4f74edea91
tanishhhqq <154842728+tanishhhqq@users.noreply.github.com>
2024-05-09 21:09:53 +0530

    Create printedpasswords.cpp

---
+++ printedpasswords.cpp
@@ -0,0 +1,42 @@
+#include <conio.h>
+#include <iostream>
+using namespace std;
+enum IN {
+IN_BACK = 8,
+IN_RET = 13
+};
+std::string takePasswdFromUser(
+char sp = '*')
+{
+  string passwd = "";
+  char ch_ipt;
+  while (true) {
+    ch_ipt = getchar();
+    if (ch_ipt == IN::IN_RET) {
+      cout << endl;
+      return passwd;
+}
+    else if (ch_ipt == IN::IN_BACK
+      && passwd.length() != 0) {
+      passwd.pop_back();
+      cout << "\b \b";
+      continue;
+}
+    else if (ch_ipt == IN::IN_BACK
+      && passwd.length() == 0) {
+      continue;
+}
+    passwd.push_back(ch_ipt);
+    cout << sp;
+  }
+}
+int main()
+{
+  string name;
+  string input;
+  cout<<"Enter the Username : ";
+  cin>>name;
+  cout<<"Enter the Password : ";
+  input = takePasswdFromUser();
+  cout << input << endl;
+}
